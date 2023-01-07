96e0b293b8b468d321942d8db9e3788c0d0323b3
Sergei <hentai.exe@gmail.com>
2023-01-08 00:48:21 +0600

    tdd

--- src/todos/topics/tdd.clj
+++ src/todos/topics/tdd.clj
@@ -29,7 +29,46 @@
     (str/join "-" [y (pad m) (pad d)])))
 
 (defn increment-day [d]
-  (doto d
-      (.add Calendar/DAY_OF_MONTH 1)))
+  (doto (.clone d)
+    (.add Calendar/DAY_OF_MONTH 1)))
 
+(defn increment-month [d]
+  (doto (.clone d)
+    (.add Calendar/MONTH 1)))
+
+(defn increment-year [d]
+  (doto (.clone d)
+    (.add Calendar/YEAR 1)))
+
+(defn decrement-day [d]
+  (doto (.clone d)
+    (.add Calendar/DAY_OF_MONTH -1)))
+
+(defn decrement-month [d]
+  (doto (.clone d)
+    (.add Calendar/MONTH -1)))
+
+(defn decrement-year [d]
+  (doto (.clone d)
+    (.add Calendar/YEAR -1)))
+
+
+;; for mocking
+
+
+(defn log-call [id & args]
+  (println "Audit: called " id " with " (str/join ", " args)))
+
+(defn fetch-all-expenses [username start-date end-date]
+  (log-call "fetch all" username start-date end-date)
+  ;; querying database
+  )
+
+(defn expenses-greater-than [expenses threshold]
+  (log-call "expenses-greater-than" threshold)
+  (filter #(> (:amount %) threshold) expenses))
+
+(defn fetch-expenses-greater-than [username start-date end-date threshold]
+  (let [all (fetch-all-expenses username start-date end-date)]
+    (expenses-greater-than all 10)))
 
