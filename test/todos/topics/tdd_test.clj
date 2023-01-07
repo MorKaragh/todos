e3ada86d956bac8daaf320fe86ada585f12c558d
Sergei <hentai.exe@gmail.com>
2023-01-08 00:48:08 +0600

    tdd

--- test/todos/topics/tdd_test.clj
+++ test/todos/topics/tdd_test.clj
@@ -13,6 +13,32 @@
     (is (= (as-string d) "2011-05-13"))))
 
 (deftest test-increment-date
-    (let [d (date "2011-05-13")
-          nth-day (increment-day d)]
-      (is (= (as-string d) "2011-05-14"))))
+  (let [d (date "2011-05-13")
+        nth-day (increment-day d)
+        nth-month (increment-month d)
+        nth-year (increment-year d)]
+    (is (= (as-string nth-day) "2011-05-14"))
+    (is (= (as-string nth-month) "2011-06-13"))
+    (is (= (as-string nth-year) "2012-05-13"))))
+
+(deftest test-decrement-date
+  (let [d (date "2011-05-13")
+        nth-day (decrement-day d)
+        nth-month (decrement-month d)
+        nth-year (decrement-year d)]
+    (is (= (as-string nth-day) "2011-05-12"))
+    (is (= (as-string nth-month) "2011-04-13"))
+    (is (= (as-string nth-year) "2010-05-13"))))
+
+
+(def expensez [{:amount 10.0 :date "2010-02-28"}
+               {:amount 20.0 :date "2010-02-25"}
+               {:amount 30.0 :date "2010-02-21"}])
+
+(deftest test-fetch-expenses-greater-than
+  (stubbing [fetch-all-expenses expensez]
+            (let [filtered (fetch-expenses-greater-than "" "" "" 15.0)]
+              (is (= (count filtered) 2))
+              (is (= (:amount (first filtered)) 20))
+              (is (= (:amount (last filtered)) 30)))))
+
