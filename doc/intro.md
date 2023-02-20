a6e4626468030473da5f00d615aaf28bee7ac49d
Sergei <hentai.exe@gmail.com>
2023-02-20 22:09:48 +0300

    xxx

--- doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to todos
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
--- src/todos/topics/memoize.clj
+++
@@ -1,9 +0,0 @@
-(ns todos.topics.memoize)
-
-(defn slow-calc [x y]
-  (Thread/sleep 1000)
-  (* x y))
-
-(def fast-calc (memoize slow-calc))
-
-(fast-calc 2 5)
